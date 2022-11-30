9fb27a77a3de4e7b6f43a9436480dbee8f1330dd
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 00:47:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 		include_once 'Rss_Parser.php';
 
 		$rss = new Rss_Parser();
-		$rss->load('http://feeds.feedburner.com/tweakers/mixed');
+		$rss->load('https://nitter.bihlink.com/abidnev/rss');
 
 		foreach($rss->getItems() as $item){
 			echo '<a href="'.$item->getLink().'">'.$item->getTitle().'</a><br />';
