221f902d70608ad1d607f635915420ebf6ae456b
bihlink <95501288+bihlink@users.noreply.github.com>
2022-11-30 23:04:01 +0100

    Update navbar.php

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,1 +1,34 @@
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3 sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://xabid.com" title="Tuby"><img src="https://blogger.googleusercontent.com/img/a/AVvXsEj5zx1zoZGjrdjHDOwoGHMWQ2TPBE53s9MfhyZwLMlIBkMLREcXz2smjiHXbfGGP9lYNkeqCfcijcNUerJ7mxJXsCl8WkBLpMN1RrCOjqetT2J_iWwBswuORUbElRMteOofDGSrt50kxUeAadncOa75kj8Jx0poKAqRwZtUON2xkSS0lzngT2bZQRCd=s150" alt="xabid" height="34"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <form class="d-flex" action="https://twitter.xabid.com/search/">
+        <input class="form-control me-2 d-lg-none d-sm-block d-sm-block" type="search" placeholder="Search Tweets" aria-label="Search" name="q" value="">
+        <button class="btn btn-secondary d-lg-none d-sm-block d-sm-block" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="https://twitter.xabid.com"><i class="bi bi-house"></i> Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://seolads.com"><i class="bi bi-star"></i> My Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.trendsmap.com/twitter/user/abidnev" target="_blank" rel="nofollow"><i class="bi bi-bar-chart-fill"></i> Stats</a>
+        </li>
+      </ul>
+      <form class="d-flex" action="https://twitter.xabid.com/search/">
+        <input class="form-control me-2 d-none d-lg-block" type="search" placeholder="Search Tweets" aria-label="Search" name="q" value="">
+        <button class="btn btn-secondary d-none d-lg-block" type="submit">Search</button>
+      </form>
+      <div class="ms-2">
+        <label><input type="checkbox" id="lightSwitch"><span class="label" title="Switch Light Mode"><i class="bi bi-brightness-high-fill"></i></span></label>
+        <script src="https://tuby.me/inc/lightswitch.js"></script>
+      </div>
+      <!--<a class="btn btn-warning ms-md-3" href="#" role="button">Submit Video</a>-->
+    </div>
+  </div>
+</nav>
