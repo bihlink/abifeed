12a3caa67b96244f276e9cdd50a6f5ce32f7df1e
bihlink <95501288+bihlink@users.noreply.github.com>
2022-11-30 23:07:03 +0100

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,80 @@
+<div class="col-lg-12 mt-3">
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-center">
+          <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+              <a href="https://www.facebook.com/trendingfuntoday/" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-facebook" style="font-size: 2rem; color: blue;"></i></a>
+              <a href="https://twitter.com/abidnev/" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-twitter" style="font-size: 2rem; color: #55acee;"></i></a>
+              <a href="https://www.reddit.com/user/abidly" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-reddit" style="font-size: 2rem; color: #dd4b39;"></i></a>
+              <a href="https://www.instapaper.com/p/anwebservices" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-instagram" style="font-size: 2rem; color: #ac2bac;"></i></a>
+              <a href="https://www.linkedin.com/in/abid-nevesinjac/" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-linkedin" style="font-size: 2rem; color: #0082ca;"></i></a>
+              <a href="https://www.pinterest.com/xabid/" role="button" rel="nofollow noopener noreferrer"><i class="bi bi-pinterest" style="font-size: 2rem; color: #cc0000;"></i></a>
+            </section>
+            <!-- Section: Social media -->
+          </div>
+          <h5 class="text-uppercase">@abidnev</h5>
+          <p>CEO / Founder of seolads.com Largest #SEO And #SocialMedia #Marketing Marketplace. #DigitalMarketing #Entrepreneur #WordPress & #Software Developer</p>
+        </div>
+        <!--Grid column-->
 
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase"><i class="bi bi-heart-fill"></i> My Services</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <i class="bi bi-cloud-arrow-up"></i> <a href="https://seolads.com/service/smm-services/sponsored-tweet-abidnev-verified-twitter-influencer/">Buy Sponsored Tweet</a>
+            </li>
+            <li>
+              <i class="bi bi-postcard"></i> <a href="https://seolads.com/service/smm-services/12-million-tweet-embeds-twitter-marketing/">Buy Twitter Promotion</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0"><i class="bi bi-diagram-3-fill"></i> My Websites</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://bihlink.com" rel="me" target="_blank">BiH Link - Search Engine</a>
+            </li>
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://tuby.me" rel="me" target="_blank">Tuby Videos</a>
+            </li>
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://upvo.me" rel="me" target="_blank">Social Bookmarks</a>
+            </li>
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://coki.me" rel="me" target="_blank">URL Shortener</a>
+            </li>
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://seolads.com" rel="me" target="_blank">SEO & SMM Marketplace</a>
+            </li>
+            <li>
+              <i class="bi bi-link-45deg"></i> <a href="https://my.coki.me" rel="me" target="_blank">Create Digital Business Card</a>
+            </li>
+
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2022 Development By 
+      <a href="https://xabid.com/" rel="me" target="_blank">Abid</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+</div>
