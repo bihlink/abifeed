c9d85090ee136e1d877586957238222f0844c5c9
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 13:50:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo file_get_contents("https://twitter.xabid.com/");
+$site = file_get_contents("https://twitter.xabid.com/");
+
+$site = str_replace('http://nitter.bihlink.com/search', '/search/', $site);
+
+echo $site;
 ?>
